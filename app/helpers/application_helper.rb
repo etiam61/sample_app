5f2e89a3856f747135cee6e19305024504057483
Van Dong <bui.van.dong@sun-asterisk.com>
2021-08-03 19:46:04 +0700

    Chapter 5: Filling in the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title page_title, base_title = t("title")
+  def full_title page_title, base_title = t(".title")
     page_title.blank? ? base_title : [page_title, base_title].join(" | ")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
-    get "static_pages/home"
-    get "static_pages/help"
+    get "/home", to: "static_pages#home"
+    get "/help", to: "static_pages#help"
+    get "/contact", to: "static_pages#contact"
+    get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
