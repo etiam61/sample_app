3e662733aa6a6ed4ca63ee245495e4493ec30047
Van Dong <bui.van.dong@sun-asterisk.com>
2021-08-04 14:46:35 +0700

    Chapter 6: Modeling User

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :name, presence: true,
+            length: {maximum: Settings.model.user.name.length.maximum}
+  validates :email, presence: true,
+            length: {maximum: Settings.model.user.email.length.maximum},
+            format: {with: Settings.model.user.email.regex_valid},
+            uniqueness: true
+  validates :password, presence: true,
+             length: {minimum: Settings.model.user.password.length.minimum}
+  has_secure_password
+
+  private
+  def downcase_email
+    email.downcase!
+  end
+end
